ed2098e883f4234d5a4050eabf035e49dbbe4be5
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-07-25 13:16:17 +0300

    First commit

---
+++ NLayerProject.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Entities
+{
+    public abstract class BaseEntity //Abstract yapıyoruz böylece new'lenemeyen bir yapı inşa ediyoruz.Soyut bir nesnedir.Ortak olan property'leri buraya yazdım.
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+
+    }
+}
