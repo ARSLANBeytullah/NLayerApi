aade73646be859b9295427c09578f9c2094279fa
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-07-25 14:17:04 +0300

    Core Layer Updated-Business folder and data folter created

---
+++ NLayerProject.Core/Business/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Business
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression); //example call:ProductRepository.Where(x=>x.id>5).OrderBy.ToListAsync();
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity); //Data access katmanın da Update ve remove methodlarının asenkron methodları yok ama servis katmanında asenkronları vardır.
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
---
+++ NLayerProject.Core/Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Data.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> GetAll(Expression<Func<T,bool>> expressin);
+        IQueryable<T> Where(Expression<Func<T,bool>> expression); //example call:ProductRepository.Where(x=>x.id>5).OrderBy.ToListAsync();
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+        
+        void Update(T entity); //Güncelleme ve silmenin asenkronu yok. Bunlar zaten çok performanslı durumlar olduğundan asenkronları yoktur.
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
