580b10b8da2b7744218a6e8945122c570cfc0608
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-07-25 15:20:41 +0300

    Added IUnitOfWork,Configurations Folder and AppDbContext class

---
+++ NLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+         
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductsFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //Bu kod bütün assembly'lerde ki(her bir katman aynı zaman da assembly olarak adlandırılır.)  
+            //Tek bir satırda bütün configuration dosyalarını okur
+            
+
+
+            //Bu şekilde tek tek yazmak yerine üstte ki tek bir satır kodla aynı şeyi yapabiliriz.
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration);
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration);    
+            //modelBuilder.ApplyConfiguration(new ProductFeatureConfiguration);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NLayerProject.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Data.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category> //Fluent Api kullanarak entity'lerimizin veritabanı ayarlarını yapıyoruz.
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
