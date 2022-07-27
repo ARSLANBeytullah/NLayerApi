aff0678d209bce4159b745dfae273d3c81c4f8c2
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-07-27 16:37:08 +0300

    Dtos Created

---
+++ NLayerProject.Core/Dtos/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Dtos
+{
+    public class ProductDto:BaseDto  //DTO'lar sadece son kullanıcıları ilgilendiren bilgiler içermeli. Navigation Property'leri içermemelidir.
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
